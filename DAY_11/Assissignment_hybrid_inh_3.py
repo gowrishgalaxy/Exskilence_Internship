4d10b00ce599c2210aeb233ca40495b7d2b3dd4b
gowrishgalaxy <gowrishhm842@gmail.com>
2026-02-19 00:33:12 +0530

    .

---
+++ DAY_11/Assissignment_hybrid_inh_3.py
@@ -0,0 +1,27 @@
+class User:
+    def __init__(self, name, **kwargs):
+        self.name = name
+
+
+class Driver(User):
+    def __init__(self, name, car, **kwargs):
+        super().__init__(name, **kwargs)
+        self.car = car
+
+
+class Rider(User):
+    def __init__(self, name, pickup_location, **kwargs):
+        super().__init__(name, **kwargs)
+        self.pickup_location = pickup_location
+
+
+class Trip(Driver, Rider):
+    def __init__(self, name, car, pickup_location):
+        super().__init__(name=name, car=car, pickup_location=pickup_location)
+
+    def summary(self):
+        return f"{self.name} will pick up the rider from {self.pickup_location} using {self.car}."
+
+
+t1 = Trip("Amit", "Honda City", "Sector 21")
+print(t1.summary())
