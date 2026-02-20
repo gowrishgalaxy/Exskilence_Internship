466489268baf47793ab6744c6fbfce6225232a1a
gowrishgalaxy <gowrishhm842@gmail.com>
2026-02-21 00:52:59 +0530

    .

---
+++ DAY_12/Assignment_multiple_inh_4.py
@@ -0,0 +1,18 @@
+class Book:
+    def read_page(self, page):
+        return f"Reading page {page}"
+
+
+class BatteryPowered:
+    def battery_status(self, level):
+        return f"Battery at {level}%"
+
+
+class EBookReader(Book, BatteryPowered):
+    pass
+
+
+e1 = EBookReader()
+
+print(e1.read_page(10))
+print(e1.battery_status(90))
