c7f165cd6ae57e4896cdd8c39271238c26c86d42
gowrishgalaxy <gowrishhm842@gmail.com>
2026-02-21 00:52:05 +0530

    .

---
+++ DAY_12/Assignment_multiple_inh_3.py
@@ -0,0 +1,18 @@
+class Flyer:
+    def fly(self, height):
+        return f"Flying at {height} meters"
+
+
+class Camera:
+    def take_photo(self, location):
+        return f"Photo taken at {location}"
+
+
+class Drone(Flyer, Camera):
+    pass
+
+
+d1 = Drone()
+
+print(d1.fly(50))
+print(d1.take_photo("mountain"))
